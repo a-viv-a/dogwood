cefcd912acdf326de3fa54cfa0edb0be02101f83
Aviva Ruben <aviva@rubenfamily.com>
2025-02-12 19:39:18 -0600

    stackhashmap pt2

--- src/stackhashmap.rs
+++ src/stackhashmap.rs
@@ -8,6 +8,7 @@ struct StackHashMap<K, V> {
 impl<K: Eq + std::hash::Hash, V> StackHashMap<K, V> {
     fn new() -> Self {
         StackHashMap {
+            // TODO: is non empty data an invarient? if not, should this start empty?
             data: vec![HashMap::new()],
         }
     }
@@ -22,16 +23,25 @@ impl<K: Eq + std::hash::Hash, V> StackHashMap<K, V> {
 
     fn pop_frame(&mut self) {
         self.data.pop();
-        // TODO: do we want this?
+        // TODO: do we want this invarient?
         assert!(!self.data.is_empty());
     }
 
     fn get(&self, k: &K) -> Option<&V> {
-        self.data.iter().rev().flat_map(|m| m.get(k)).next()
+        self.data.iter().rev().flat_map(|hm| hm.get(k)).next()
     }
 
     fn insert(&mut self, k: K, v: V) -> Option<V> {
+        // TODO: if not being empty is an invarient we could unwrap / expect here
         self.data.last_mut().and_then(|hm| hm.insert(k, v))
+    }
+
+    fn scope<F: FnOnce(&mut Self)>(&mut self, f: F) {
+        let prev_height = self.height();
+        self.push_frame();
+        f(self);
+        self.pop_frame();
+        assert_eq!(self.height(), prev_height);
     }
 }
 
@@ -82,5 +92,32 @@ mod tests {
 
         shm.pop_frame();
         assert_eq!(*shm.get(&"k").unwrap(), 5);
+    }
+
+    #[test]
+    fn scope() {
+        let mut shm = StackHashMap::new();
+
+        shm.insert(0, true);
+        shm.insert(1, true);
+        assert_eq!(*shm.get(&1).unwrap(), true);
+        assert_eq!(shm.height(), 1);
+
+        shm.scope(|shm| {
+            assert_eq!(shm.height(), 2);
+            shm.insert(1, false);
+            assert_eq!(*shm.get(&1).unwrap(), false);
+        });
+
+        assert_eq!(*shm.get(&1).unwrap(), true);
+        assert_eq!(shm.height(), 1);
+        shm.scope(|shm| {
+            shm.scope(|shm| {
+                shm.scope(|shm| {
+                    assert_eq!(shm.height(), 4);
+                    assert_eq!(*shm.get(&1).unwrap(), true);
+                })
+            })
+        })
     }
 }
